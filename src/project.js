46b93c9cea1d0c890adda711504ebaf7f86cfbc3
Joe Turner <joe@oampo.co.uk>
2018-02-21 14:46:53 +0000

    Get route param

--- src/project.js
+++ src/project.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Project(props) {
-    return <div>Welcome to project {props.name}</div>;
+    return <div>Welcome to project {props.match.params.projectName}</div>;
 }
