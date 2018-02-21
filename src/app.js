0e515edef4c75ab34393fd1bfbeab94a2ac0506b
Joe Turner <joe@oampo.co.uk>
2018-02-21 14:41:09 +0000

    Set up the routes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route} from 'react-router';
 import NavBar from './nav-bar';
 import HomePage from './home-page';
 import ProjectsNav from './projects-nav';
@@ -15,10 +16,10 @@ export default function App(props) {
     return (
         <div>
             <NavBar />
-            <HomePage />
-            <ProjectsNav />
-            <ProjectsPage />
-            <Project name="foo" />
+            <Route path="/" component={HomePage} exact />
+            <Route path="/projects" component={ProjectsNav} />
+            <Route path="/projects" component={ProjectsPage} exact />
+            <Route path="/projects/:projectName" component={Project} exact />
         </div>
     );
 }
